7fe94e85381b29c0eab0ee469ed31b3fd66954d6
rajan <rajanmalik881@gmail.com>
2022-08-14 21:56:18 +0530

    Tags Changes

---
+++ CucumberProject/src/test/java/com/StepDefination/TagsDemo.java
@@ -0,0 +1,36 @@
+package com.StepDefination;
+
+import io.cucumber.java.en.Given;
+
+public class TagsDemo {
+
+	@Given("This is Scenario1")
+	public void this_is_scenario1() {
+
+	}
+
+	@Given("This is Scenario2")
+	public void this_is_scenario2() {
+
+	}
+
+	@Given("This is Scenario3")
+	public void this_is_scenario3() {
+
+	}
+
+	@Given("This is Scenario4")
+	public void this_is_scenario4() {
+
+	}
+
+	@Given("This is Scenario5")
+	public void this_is_scenario5() {
+
+	}
+
+	@Given("This is Scenario6")
+	public void this_is_scenario6() {
+
+	}
+}
---
+++ CucumberProject/src/test/resources/Features/TagsDemo.feature
@@ -0,0 +1,20 @@
+Feature: Use of Tags in Cucumber
+
+    @SmokeTest
+    Scenario: Scenario1        
+    Given This is Scenario1
+    @SmokeTest
+    Scenario: Scenario2        
+    Given This is Scenario2
+    
+    Scenario: Scenario3        
+    Given This is Scenario3
+    
+    Scenario: Scenario4        
+    Given This is Scenario4
+    
+    Scenario: Scenario5        
+    Given This is Scenario5
+    
+    Scenario: Scenario6        
+    Given This is Scenario6
